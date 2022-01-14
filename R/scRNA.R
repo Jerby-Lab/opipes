ed7221b01ac36d58db87149a7ac6d0f987ba5dd6
Christine Yeh <christineyyeh@gmail.com>
2022-01-14 14:57:10 -0800

    add embedding functions

--- R/scRNA.R
+++ R/scRNA.R
@@ -1,28 +1,162 @@
-subsample <- function(counts, metadata){
-  counts
-}
-
-#' This function loads a file as a matrix. It assumes that the first column
-#' contains the rownames and the subsequent columns are the sample identifiers.
-#' Any rows with duplicated row names will be dropped with the first one being
-#' kepted.
+#' Randomly subsample single cell data set
+#' 
+#' Randomly subsample a proportion of data, 
+#' cells must be in the same order in counts 
+#' and metadata. 
 #'
 #' @param counts a _m_ x _n_ matrix of raw count data where _m_ = no. genes, and _n_ = no. cells 
-#' @param prj_name project name string 
+#' @param metadata matrix _n_ x _j_ of metadata where _n_ = no. cells, and _j_ = no. features
+#' @param p proportion of data to subsample (p = [0, 1])
+#' @return a list with objects named "c" and "m" corresponding to your subsampled counts and metadata 
+#' @export
+
+subsample <- function(counts, metadata, p=0.5){
+  n_cells <- dim(counts)[2]
+  cell_idx <- sample(1:n_cells, size = p*n_cells)
+  subsample_counts <- counts[, cell_idx]
+  subsample_meta <- metadata[cell_idx,]
+  out <- list()
+  out[["c"]] <- subsample_counts
+  out[["m"]] <- subsample_meta
+  return(out)
+}
+
+#' Seuratify your Single Cell Data 
+#' 
+#' This function takes in raw counts data and 
+#' associated metadata and creates a SeuratObject
+#'
+#' @param counts a _m_ x _n_ matrix of raw count data where _m_ = no. genes, and _n_ = no. cells 
 #' @param metadata matrix of metadata with at least a column cell identifier named "cellid" 
+#' @param prj_name project name string 
 #' @return a Seurat object of your count matrix with metadata 
 #' @export
-seuratify <- function(counts, prj_name, metadata){
+seuratify <- function(counts, metadata, prj_name = ""){
+  start_time <- Sys.time()
+  
   so <- Seurat::CreateSeuratObject(counts, project = prj_name)
   so@meta.data$cellid <- row.names(so@meta.data)
-  so@meta.data <- dplyr::merge(so@meta.data, metadata, by.x = "cellid", all.x = T, sort = F)
+  # so@meta.data <- data.table::merge.data.table(so@meta.data, metadata, by = "cellid", all.x = T, sort = F)
+  
+  end_time <- Sys.time()
+  run_time <- end_time - start_time
+  print(run_time)
+  
   return(so)
 }
 
-# std_preprocess
+#' Standard single cell RNA preprocessing 
+#' 
+#' normalize, transform, and scale
+#'
+#' @param so a SeuratObject of your data 
+#' @return a preprocessed SeuratObject
+#' @export
+#' 
+std_preprocess <- function(so){
+  start_time <- Sys.time()
 
-# embed
+  so <- Seurat::NormalizeData(so)
+  so <- Seurat::FindVariableFeatures(so, selection.method = "vst", nfeatures = 2000)
+  so <- Seurat::ScaleData(so)
+  
+  end_time <- Sys.time()
+  run_time <- end_time - start_time
+  print(run_time)
+  return(so)
+}
 
+#' Embedding methods: PCA, tSNE, UMAP
+#' 
+#' functionality for returning PCA, tSNE, 
+#' and UMAP projections 
+#'
+#' @param so a SeuratObject of your data 
+#' @param approx_pca.flag approximate PCA calculation? 
+#' @param tsne.flag boolean, run tSNE? 
+#' @param umap.flag boolean, run umap?
+#' @param n_pcs number of PCs to run umap and tsne on 
+#' @param resolution clustering resolution, default = 0.4
+#' @param plot plot initial clustering?
+#' @return a list of all parameters calculated 
+#' @export
+#' 
+embed <- function(so,
+                  approx_pca.flag = T 
+                  tsne.flag = F,
+                  umap.flag = F,
+                  n_pcs = 30,
+                  resolution = 0.4,
+                  plot = T){
+  out <- list()
+  
+  # run pca
+  start_time <- Sys.time()
+  print("Running PCA....")
+  so <- Seurat::RunPCA(so, npcs= n_pcs, approx = approx_pca.flag)
+  
+  # calculate variance explained
+  mat <- Seurat::GetAssayData(so, assay = "RNA", slot = "scale.data")
+  total_variance <- sum(matrixStats::rowVars(mat))
+  eigValues = (so[["pca"]]@stdev)^2  ## EigenValues
+  varExplained = eigValues / total_variance
+  
+  print(paste0("PC1 Variance Explained: ", round(varExplained[1]*100, 2), "%"))
+  print(paste0("PC2 Vairance Explained: ", round(varExplained[2]*100, 2), "%"))
+  print(paste0("Variance Explained in ", n_pcs, " PCs : ", round(sum(varExplained*100), 2), "%"))
+  
+  end_time <- Sys.time()
+  run_time <- end_time - start_time
+  print(run_time)
+  
+  out[["pca"]] <- so[["pca"]]@cell.embeddings
+  
+  print(run_time)
+  
+  # run tsne
+  if (tsne.flag){
+    start_time <- Sys.time()
+    
+    print("Running TSNE...")
+    so <- Seurat::RunTSNE(object = so, reduction.use = "pca",dims = 1:n_pcs, do.fast = TRUE)
+     out[["tsne"]] <- so[["tsne"]]@cell.embeddings
+    
+    end_time <- Sys.time()
+    run_time <- end_time - start_time
+    print(run_time)
+  }
+  
+  # run umap 
+  if(umap.flag){
+    start_time <- Sys.time()
+    
+    print("Running UMAP...")
+    so <- Seurat::RunUMAP(object = so, dims = 1:n_pcs, )
+    out[["umap"]] <- so[["umap"]]@cell.embeddings
+    
+    end_time <- Sys.time()
+    run_time <- end_time - start_time
+    print(run_time)
+  }
+  
+  # clustering
+  print("Running Clustering...")
+  start_time <- Sys.time()
+  so <- FindNeighbors(so, reduction = "pca",dims = 1:n_pcs)
+  so <- FindClusters(so, resolution = resolution)
+  end_time <- Sys.time()
+  run_time <- end_time - start_time
+  print(run_time)
+  
+  if (plot) {
+    if (tsne.flag) {
+      Seurat::DimPlot(so, reduction = "tsne", group.by = "orig.ident") 
+    }
+    if (umap.flag) {
+      Seurat::DimPlot(so, reduction = "umap", group.by = "cluster") 
+    }
+  }
+}
 
-
+search_for_cluster_resolution()
 
